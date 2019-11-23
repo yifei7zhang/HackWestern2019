ae99189db903ff7857ed94e47c56a82348bc7020
Kairui Zeng <zengkr@gmail.com>
2019-11-23 18:09:34 -0500

    updated openData API requests

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
-
-
+const Promise = require('promise');
+const rp = require('request-promise');
+/*
 // const hostname = '127.0.0.1';
 // const port = 3000;
 
@@ -28,18 +29,88 @@ MongoClient.connect(uri, function(err, client) {
    // perform actions on the collection object
    client.close();
 });
+*/
+
+// ************************ Everything above this is a place into database function? ***********************************
+// ************************ Everything above this is a place into database function? ***********************************
+// ************************ Everything above this is a place into database function? ***********************************
+// ************************ Everything above this is a place into database function? ***********************************
+// ************************ Everything above this is a place into database function? ***********************************
 
 
-request('https://maps.london.ca/arcgisa/rest/services/OpenData/OpenData_Transportation/MapServer/19/query?where=1%3D1&outFields=*&outSR=4326&f=json', { json: true }, (err, res, body) => {
+// Grab API data for streetlights functions
+async function loadLight(){
+  await rp('https://opendata.arcgis.com/datasets/e2db218c663f4b9f9210150513a6c54a_19.geojson', { json: true }, (err, res, body) => {
   if (err) { 
-    return console.log(err); 
-  }
-  else {
-    for (var thing in body.features) {
-      console.log(body.features[thing].geometry);
+      return console.log(err); 
     }
-    // console.log(JSON.parse(body));
-  }
-  
-  
-});
+    else {
+      return body;
+    }
+  });
+}
+
+async function loadEmergency(){
+  await rp('https://opendata.arcgis.com/datasets/174ed0d5be31424dab612268db1fc460_1.geojson', { json: true }, (err, res, body) => {
+  if (err) { 
+      return console.log(err); 
+    }
+    else {
+      return body;
+    }
+  });
+}
+async function loadHospitals(){
+  await rp('https://opendata.arcgis.com/datasets/a1ca22a405fe4ad9852477a7add40565_2.geojson', { json: true }, (err, res, body) => {
+  if (err) { 
+      return console.log(err); 
+    }
+    else {
+      return body;
+    }
+  });
+}
+
+async function loadSchools(){
+  await rp('https://opendata.arcgis.com/datasets/cabb026f825348ae9c606f2c8b013e21_4.geojson', { json: true }, (err, res, body) => {
+  if (err) { 
+      return console.log(err); 
+    }
+    else {
+      return body;
+    }
+  });
+}
+
+async function loadPaths(){
+  await rp('https://opendata.arcgis.com/datasets/a1c044cbeb5c4030909764177cd35a26_2.geojson', { json: true }, (err, res, body) => {
+  if (err) { 
+      return console.log(err); 
+    }
+    else {
+      return body;
+    }
+  });
+}
+
+async function loadAll(){
+
+  var lightArray = await loadLight();
+  console.log(lightArray[0]);
+  var eServiceArray = await loadEmergency();
+  var hospitalArray = await loadHospitals();
+  var schoolArray = await loadSchools();
+  var pathArray = await loadPaths();
+  var multipliers = setBaseMultipliers(lightArray, eServiceArray, hospitalArray, schoolArray, pathArray);
+}
+
+async function setBaseMultipliers(lightArray, eServiceArray, hospitalArray, schoolArray, pathArray){
+  var xCoord, yCoord;
+  /*for (int i = 0; i < lightArray.length; ++i){
+    xCoord = lightArray.features[i].geometry;
+    console.log(xCoord);
+    
+  }*/
+}
+
+loadAll();
